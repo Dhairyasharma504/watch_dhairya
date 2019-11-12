37bb96a07c415399920fba3c5771c8b30544148c
Dhairyasharma504 <you@example.com>
2019-11-12 13:19:58 +0530

    icons

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -15,6 +15,11 @@ const Wrapper = styled.div`
     background-color: #f9f9ff;
     border-radius: 10px;
     box-shadow: 0px 0px 8px -32px wheat;
+    padding: 32px;
+  }
+  .icon {
+    color: blueviolet;
+    margin-right: 3px;
   }
 `;
 const Feature = () => (
@@ -23,7 +28,7 @@ const Feature = () => (
       <body>
         <section className="section">
           <div className="container">
-            <h3 className="title is-spaced is-size-2 has-text-weight-bold has-text-centered  ">
+            <h3 className="title is-spaced is-size-2 has-text-weight-bold has-text-centered is-size-1-mobile ">
               Some Features that Made us Unique
             </h3>
             <p className="subtitle has-text-weight-light is-spaced  has-text-centered	">
@@ -32,41 +37,45 @@ const Feature = () => (
             <div className="columns">
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Expert Technicians
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="far fa-user" />
+                    </span>
+                    Expert Technicians
+                  </h3>
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Professional Service
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="fas fa-id-card" />
+                    </span>
+                    Professional Service
+                  </h3>
+
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Great Support
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="fas fa-phone-volume" />
+                    </span>
+                    Great Support
+                  </h3>
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
             </div>
@@ -74,41 +83,44 @@ const Feature = () => (
             <div className="columns">
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Technical Skills
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="fas fa-cogs" />
+                    </span>
+                    Technical Skills
+                  </h3>
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Highly Recomended
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="far fa-gem" />
+                    </span>
+                    Highly Recomended
+                  </h3>
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
               <div className="column">
                 <div className="card">
-                  <section className="section">
-                    <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
-                      Positive Reviews
-                    </h3>
-                    <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
-                      Usage of the Internet is becoming more common due to rapid
-                      advancement of technology and power.
-                    </p>
-                  </section>
+                  <h3 className="title is-spaced is-size-5 has-text-weight-bold has-text-left  ">
+                    <span className="icon">
+                      <i className="fab fa-rocketchat" />
+                    </span>
+                    Positive Reviews
+                  </h3>
+                  <p className="subtitle has-text-weight-light is-spaced  has-text-left is-size-6 is-family-sans-serif">
+                    Usage of the Internet is becoming more common due to rapid
+                    advancement of technology and power.
+                  </p>
                 </div>
               </div>
             </div>
